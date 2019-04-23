0751c685f6d62784973b9d6b4ab7460048dccb5d
Richardson Bruno <richardsonbrc@gmail.com>
2019-04-23 12:31:22 -0300

    Express passando parametros na url e json

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 
-http
-  .createServer((req, res) => {
-    console.log(req);
-    return res.end("Hello World!");
-  })
-  .listen(3000);
+app.get("/", (req, res) => {
+  return res.send(`Seja Bem Vindo, ${req.query.name}`);
+});
+
+app.get("/login", (req, res) => {
+  return res.send("Login");
+});
+
+app.get("/nome/:name", (req, res) => {
+  return res.send(`Seja bem vindo, ${req.params.name}`);
+});
+
+app.get("/json/:name", (req, res) => {
+  return res.json({
+    message: `Seja Bem Vindo, ${req.params.name}`
+  });
+});
+
+app.listen(3000);
