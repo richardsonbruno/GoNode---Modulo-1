1307edcdf11f164f20d7631f100352d26c6a1346
Richardson Bruno <richardsonbrc@gmail.com>
2019-04-23 19:21:20 -0300

    Configurando Nunjucks, Render e Send Form

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
 const express = require("express");
+const nunjucks = require("nunjucks");
+
 const app = express();
 
-const logMiddleware = (req, res, next) => {
-  console.log(
-    `HOST: ${req.headers.host} | URL: ${req.url} | METHOD: ${req.method}`
-  );
-
-  req.appName = "GoNode";
-
-  return next();
-};
-
-app.get("/", logMiddleware, (req, res) => {
-  return res.send(`Seja Bem Vindo, ${req.query.name}`);
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+  watch: true
 });
 
-app.get("/login", logMiddleware, (req, res) => {
-  return res.send(`Bem vindo ao ${req.appName}`);
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "njk");
+
+const users = [
+  "Richardson Bruno",
+  "Tais Natielle",
+  "Adileide Novaes Ramos",
+  "Carlos Roberto de Carvalho"
+];
+
+app.get("/", (req, res) => {
+  return res.render("list", { users });
 });
 
-app.get("/nome/:name", (req, res) => {
-  return res.send(`Seja bem vindo, ${req.params.name}`);
+app.get("/new", (req, res) => {
+  return res.render("new");
 });
 
-app.get("/json/:name", (req, res) => {
-  return res.json({
-    message: `Seja Bem Vindo, ${req.params.name}`
-  });
+app.post("/create", (req, res) => {
+  users.push(req.body.nome);
+  return res.redirect("/");
 });
 
 app.listen(3000);
